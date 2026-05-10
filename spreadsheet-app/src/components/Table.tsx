6c2421ad84e92a69877d39dffdeed8926c7cb2ad
Seymur <seymursuleymanov20@gmail.com>
2026-05-10 16:27:09 +0700

    реалезовано выделение ячеек через шифт

--- spreadsheet-app/src/components/Table.tsx
+++ spreadsheet-app/src/components/Table.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./Table.css";
 
+//создание таблицы
 function createTable(row: number, col: number) {
     const arr: string[][] = [];
     for (let i = 0; i < row; i++) {
@@ -12,6 +13,7 @@ function createTable(row: number, col: number) {
     return arr;
 }
 
+//функ-ция для заголовка таблиц
 function getColumnLetter(colIndex: number): string {
     let result = "";
     let num = colIndex;
@@ -22,11 +24,32 @@ function getColumnLetter(colIndex: number): string {
     return result;
 }
 
+//функция для логики выделения ячеек через shift
+
+function isRange(cell, range) {
+    if (!range) return false;
+
+    const [r1, c1] = range.start.split("-").map(Number);
+    const [r2, c2] = range.end.split("-").map(Number);
+    const [r, c] = cell.split("-").map(Number);
+
+    const minRow = Math.min(r1, r2);
+    const maxRow = Math.max(r1, r2);
+
+    const minCol = Math.min(c1, c2);
+    const maxCol = Math.max(c1, c2);
+
+    return r >= minRow && r <= maxRow && c >= minCol && c <= maxCol;
+};
+
+//основная функц-ия
 function Table() {
     const [table, setTable] = useState(createTable(26, 100));
     const [select, setSelect] = useState(null);
     const [editing, setEditing] = useState(null);
     const [editValue, setEditValue] = useState("");
+    const [lastCell, setLastCell] = useState(null);
+    const [range, setRange] = useState(null);
 
     const startEdit = (row, col, currentValue) => {
         setEditing({ row, col });
@@ -67,7 +90,7 @@ function Table() {
                         
                 {row.map((cell, colIndex) => {
                     const isEditing = editing?.row === rowIndex && editing?.col === colIndex;
-                    
+                    //редактирование
                     if (isEditing) {
                         return (
                             <input
@@ -81,13 +104,28 @@ function Table() {
                             />
                         );
                     }
-                    
+                    //вывод
                     return (
                         <button 
+                            onClick={(e) => {
+                                const current = `${rowIndex}-${colIndex}`;
+                                
+                                if (e.shiftKey && lastCell) {
+                                    setRange({ start: lastCell, end: current });
+                                    setSelect(current);
+                                } else {
+                                    setSelect(current);
+                                    setLastCell(current);
+                                    setRange(null);
+                                }
+                            }}
                             onDoubleClick={() => startEdit(rowIndex, colIndex, table[rowIndex][colIndex])}
-                            onClick={() => setSelect(`${rowIndex}-${colIndex}`)}
                             key={`${rowIndex}-${colIndex}`} 
-                            className={`cell-button ${select === `${rowIndex}-${colIndex}` ? "select" : ""}`}>
+                            className={`cell-button ${
+                                select === `${rowIndex}-${colIndex}` || isRange(`${rowIndex}-${colIndex}`, range) 
+                                    ? "select" 
+                                    : ""
+                            }`}>
                             {table[rowIndex][colIndex]}
                         </button>
                     );
